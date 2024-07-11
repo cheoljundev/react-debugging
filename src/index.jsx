1ba582180398fafa82791f08a4bcec142f228744
cheoljundev <cheoljundev@gmail.com>
2024-07-11 11:47:08 +0900

    리액트의 Strict Mode(엄격모드) 이해하기

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
+import {StrictMode} from "react";
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // StrictMode는 개발 상황에서만 두번씩 실행해줘서 에러를 처음부터 화면에서 확인할 수 있도록 돕는다.
+    // 이렇게 하면 값을 수정하면 에러가 발생하는 상황을 첫 화면에서 잡을 수 있다.
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
